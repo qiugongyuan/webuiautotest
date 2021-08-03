55afb649fcf44fbddfc824ea1708a5431305998d
qiugongyuan <360960443@qq.com>
2021-08-03 13:33:03 +0800

    selenium new

---
+++ WebUISample/WebUISample/checkbox.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import os,time
+
+driver=webdriver.Chrome()
+
+filepath=''+os.path.abspath('checkbox.html')
+print (filepath)
+#方法一
+driver.get(filepath)
+#方法一
+# inputs=driver.find_elements_by_tag_name('input')
+# for i in inputs:
+#     if i.get_attribute('type')=='checkbox':
+#         i.click()
+#         time.sleep(1)
+
+checkboxes=driver.find_elements_by_css_selector('input[type=checkbox]')
+print (len(checkboxes))
+for checkbox in checkboxes:
+    checkbox.click()
+    time.sleep(1)
+driver.find_elements_by_css_selector('input[type=checkbox]').pop().click()
+#pop()方法用于获取列表中的一个元素,默认为最后一个元素
+driver.quit()
---
+++ WebUISample/WebUISample/frame.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import os,time
+
+driver=webdriver.Chrome()
+file_path=''+os.path.abspath('frame.html')
+driver.get(file_path)
+driver.maximize_window()
+time.sleep(1)
+driver.switch_to.frame('if')
+driver.find_element_by_link_text('注册/登录').click()
+time.sleep(1)
+driver.quit()
